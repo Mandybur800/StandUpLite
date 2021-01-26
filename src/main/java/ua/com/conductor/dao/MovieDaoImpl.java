0bae07d0931b70cf827b0298e8b0632afa6b114b
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-26 14:56:39 +0200

    try-with-resourses was changed

--- src/main/java/ua/com/conductor/dao/MovieDaoImpl.java
+++ src/main/java/ua/com/conductor/dao/MovieDaoImpl.java
@@ -13,17 +13,20 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            Long itemId = (Long) session.save(movie);
+            session.save(movie);
             transaction.commit();
-            movie.setId(itemId);
             return movie;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
             throw new RuntimeException("Can't insert movie entity", e);
+        } finally {
+            session.close();
         }
     }
 
