8b12a7b25e7dbb491223bc8b22ee716ae3103f8e
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-02-04 11:34:09 +0200

    Order model was added, and service + DAO for it.

---
+++ src/main/java/ua/com/conductor/service/impl/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package ua.com.conductor.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import ua.com.conductor.dao.OrderDao;
+import ua.com.conductor.lib.Inject;
+import ua.com.conductor.lib.Service;
+import ua.com.conductor.model.Order;
+import ua.com.conductor.model.ShoppingCart;
+import ua.com.conductor.model.User;
+import ua.com.conductor.service.OrderService;
+import ua.com.conductor.service.ShoppingCartService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
+        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
+        order.setUser(shoppingCart.getUser());
+        orderDao.add(order);
+        shoppingCartService.clear(shoppingCart);
+        return order;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getOrdersHistory(user);
+    }
+}
