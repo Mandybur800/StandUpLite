4076c17405c3076c39ac4e386477553cb3954347
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-02-11 16:45:04 +0200

    controller was improved

--- src/main/java/ua/com/conductor/controllers/HelloController.java
+++ src/main/java/ua/com/conductor/controllers/HelloController.java
@@ -1,12 +1,10 @@
 package ua.com.conductor.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
     @GetMapping("/hi")
     public String sayHello() {
         return "Hello";
