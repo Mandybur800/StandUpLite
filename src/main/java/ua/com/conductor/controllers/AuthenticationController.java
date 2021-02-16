8075f86c14d72190e9d3b9b44285605e1ffeddf2
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-02-16 15:41:14 +0200

    checkstyle was fixed

--- src/main/java/ua/com/conductor/controllers/AuthenticationController.java
+++ src/main/java/ua/com/conductor/controllers/AuthenticationController.java
@@ -3,11 +3,9 @@ package ua.com.conductor.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.com.conductor.model.dto.UserRequestDto;
 import ua.com.conductor.security.AuthenticationService;
-import ua.com.conductor.service.dtomappers.UserMapper;
 
 @RestController
 public class AuthenticationController {
